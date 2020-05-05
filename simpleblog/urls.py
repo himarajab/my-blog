c727373c620820b707e8032489d574f064b65c13
himarajab <rajabhima2@gmail.com>
2020-05-05 22:46:35 +0200

    second episode

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView,ArticleDetailView
+# from .views import HomeView,ArticleDetailView
+from .views import *
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
-    path('article/<int:pk>',ArticleDetailView.as_view(),name='article_details'),
+    path('article/<int:pk>',ArticleDetailView.as_view(),name='article-details'),
+    path('add_post/',AddPostView.as_view(),name='add-post'),
 ]
